e4f7efe85d6e29cec5720307a7cb7fa6b423f7ae
jon atlas <JonAtlas@jons-MacBook-Air.local>
2015-09-10 17:07:55 -0400

    Creating new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
@@ -16,4 +24,13 @@ class UsersController < ApplicationController
 
   def index
   end
-end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+
+
+
+  end
