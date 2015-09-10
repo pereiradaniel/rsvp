211163a91094921f0f63deb599580acaa4fe4bdc
Daniel Pereira <pereiradpg@yahoo.ca>
2015-09-10 16:24:45 -0400

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+end
