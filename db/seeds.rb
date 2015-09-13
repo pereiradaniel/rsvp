b745420ade4e5ec6128f33cb69f777c3270e5595
jon atlas <JonAtlas@jons-MacBook-Air.local>
2015-09-13 16:05:11 -0400

    updated seeds file for new restaurant paramaters

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,13 @@ User.create!(name: "super", email: "super@mail.com", password: "password", passw
 User.create!(name: "man", email: "man@mail.com", password: "password", password_confirmation: "password")
 User.create!(name: "jon", email: "jon@mail.com", password: "password", password_confirmation: "password")
 
-Restaurant.create!(name: "Mac Donalds", capacity: 10)
-Restaurant.create!(name: "Henry's", capacity: 100)
-Restaurant.create!(name: "Burgerlings", capacity: 30)
+Restaurant.create!(name: "MacDonalds", capacity: 10, province: "Ontario", city: "Toronto", street: "Yonge", unit: "55", phone_number: 416456789)
+Restaurant.create!(name: "Henry's", capacity: 100,  province: "Alberta", city: "Edmonton", street: "King", unit: "88", phone_number: 780456789)
+Restaurant.create!(name: "Burgerlings", capacity: 30,  province: "Quebec", city: "Montreal", street: "Lange", unit: "444", phone_number: 776456789)
 
 Reservation.create!(date_time: DateTime.now, party_size: "8", user_id: 1, restaurant_id: 1)
 Reservation.create!(date_time: DateTime.now, party_size: "6", user_id: 2, restaurant_id: 2)
 Reservation.create!(date_time: DateTime.now, party_size: "7", user_id: 3, restaurant_id: 3)
+
+
+
